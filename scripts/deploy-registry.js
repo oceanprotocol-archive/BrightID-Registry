9f4db81ba7a869e18f669e7e5f43b93a14533055
trizin <25263018+trizin@users.noreply.github.com>
2021-12-07 17:40:46 +0300

    verifier wallet

--- scripts/deploy-registry.js
+++ scripts/deploy-registry.js
@@ -2,7 +2,7 @@ const hre = require("hardhat");
 
 const CONTEXT =
   "0x4f6365616e44414f000000000000000000000000000000000000000000000000";
-const VERIFIER = "0x94B805Ab1bd2028A67cD11288042bECb1E2bd531";
+const VERIFIER = "0xb1d71F62bEe34E9Fc349234C201090c33BCdF6DB";
 
 async function main() {
   let brightIdUserRegistry = await (
